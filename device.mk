33fe378467eda955114c5c17d8fa40952286f664
bengris32 <bengris32@protonmail.ch>
2022-09-18 23:03:38 +0100

    a32: overlay: Define device hardware keys configuration
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -176,7 +176,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 EVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay \
