710248d9cef751cc6c7f0bccd2909ae44b042a6a
AndroidHQ254 <d.murimi14@gmail.com>
2023-10-12 16:28:12 +0300

    a32: Import CarrierConfig from stock U6 firmware

--- device.mk
+++ device.mk
@@ -199,6 +199,7 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
+    CarrierConfigOverlayA32 \
     FrameworksResOverlayA32 \
     SettingsOverlayA32 \
     SettingsProviderOverlayA32 \
