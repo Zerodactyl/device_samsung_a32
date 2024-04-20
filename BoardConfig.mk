289214aa3b585b50019b26d3fa3420ba4cb43e8b
AndroidHQ254 <d.murimi14@gmail.com>
2024-04-20 19:23:06 +0300

    a32: sepolicy: Allow vendor_init to set vendor_mtk_camera_prop
    I auditd  : type=1107 audit(0.0:12): uid=0 auid=4294967295 ses=4294967295 subj=u:r:init:s0 msg='avc:  denied  { set } for property=ro.mtk_cam_stereo_camera_support pid=1 uid=0 gid=0 scontext=u:r:vendor_init:s0 tcontext=u:object_r:default_prop:s0 tclass=property_service permissive=1'

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,6 +125,8 @@ VENDOR_SECURITY_PATCH := 2023-11-01
 # SELinux
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
