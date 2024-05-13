bd5c1dbbd6a694a4ff3a8ead17c08d236f142ba1
AndroidHQ254 <d.murimi14@gmail.com>
2024-05-14 02:01:20 +0300

    a32: Build some Camera packages from source

--- device.mk
+++ device.mk
@@ -75,6 +75,13 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.device@3.6.vendor \
+    android.hardware.camera.provider@2.6.vendor \
+    android.frameworks.cameraservice.common@2.0.vendor \
+    android.frameworks.cameraservice.device@2.1.vendor \
+    android.frameworks.cameraservice.service@2.2.vendor
+
+PRODUCT_PACKAGES += \
     libgui_vendor
 
 # CAS
