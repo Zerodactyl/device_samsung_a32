7e3e8a846988221e242fe5a3402b3943daac030e
AndroidHQ254 <d.murimi14@gmail.com>
2024-05-16 01:12:00 +0300

    a32: Build graphics allocator & mapper 2.0

--- device.mk
+++ device.mk
@@ -103,10 +103,12 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.allocator@2.0.vendor \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.allocator@3.0.vendor \
     android.hardware.graphics.allocator@4.0.vendor \
     android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.mapper@2.0-impl \
     android.hardware.graphics.mapper@2.1.vendor \
     libvulkan
 
