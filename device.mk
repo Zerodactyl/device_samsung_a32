f4bb92d6ca283de132d751d6e4c86f71093c6a00
Michael Bestas <mkbestas@lineageos.org>
2025-02-28 01:21:09 +0200

    a32: Enable UFFD GC
    Kernel has support for it now.
    
    Change-Id: I3863ee71477f321fe47646571b857fdc39a1902b

--- device.mk
+++ device.mk
@@ -468,6 +468,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3.vendor
 
+# UFFD GC
+PRODUCT_ENABLE_UFFD_GC := true
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.samsung \
